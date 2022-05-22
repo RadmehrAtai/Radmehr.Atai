a7e7bc07afd69755af3f85212a7b0cb740d18276
RadmehrAtai <92088576+RadmehrAtai@users.noreply.github.com>
2022-05-22 22:07:07 +0430

    Add files

--- src/src/Entity/Form.php
+++ src/src/Entity/Form.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\FormRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: FormRepository::class)]
 class Form
@@ -14,12 +15,16 @@ class Form
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
     private $name;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank]
     private $email;
 
     #[ORM\Column(type: 'text')]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 3)]
     private $message;
 
     public function getId(): ?int
